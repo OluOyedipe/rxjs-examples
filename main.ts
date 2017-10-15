c10df28d6b9d48e9cfcced524f5eb916c85326c0
Olumide Oyedipe <olu@Olumides-MacBook-Pro.local>
2017-10-15 16:29:55 -0700

    Observing changes async

--- main.ts
+++ main.ts
@@ -3,11 +3,21 @@ import "rxjs/add/observable/from";
 
 let numbers = [1, 5, 10];
 let source = Observable.create((observer) => {
-    for(let n of numbers) {
 
-        observer.next(n);
-    }
-    observer.complete();
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if(index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        } else {
+            observer.complete();
+        }
+    };
+
+    produceValue();
+
+
 });
 
 source.subscribe(
